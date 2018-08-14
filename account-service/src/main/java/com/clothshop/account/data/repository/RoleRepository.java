3f3b5486a04d6be1b689158a40f4972ea22d6b3f
Rafael Manuel <erafaelmanuel@gmail.com>
2018-08-14 20:37:05 +0800

    Create role service

--- account-service/src/main/java/com/clothshop/account/data/repository/RoleRepository.java
+++ account-service/src/main/java/com/clothshop/account/data/repository/RoleRepository.java
@@ -2,9 +2,14 @@ package com.clothshop.account.data.repository;
 
 import com.clothshop.account.data.entity.Role;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface RoleRepository extends PagingAndSortingRepository<Role, String>, JpaSpecificationExecutor<Role> {
 
-    Role findByName(String name);
+    @Query("select r from Role as r where r.name = :name")
+    Optional<Role> findByName(@Param("name") String name);
 }
---
+++ account-service/src/main/java/com/clothshop/account/service/RoleService.java
@@ -0,0 +1,47 @@
+package com.clothshop.account.service;
+
+import com.clothshop.account.data.entity.Role;
+import com.clothshop.account.data.repository.RoleRepository;
+import com.clothshop.account.exception.EntityException;
+import com.clothshop.account.util.UUIDGenerator;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+public class RoleService {
+
+    private RoleRepository roleRepo;
+
+    public RoleService(RoleRepository roleRepo) {
+        this.roleRepo = roleRepo;
+    }
+
+    public Page<Role> findAll(Specification<Role> specification, Pageable pageable) {
+        return roleRepo.findAll(specification, pageable);
+    }
+
+    public Role findById(String roleId) {
+        return roleRepo.findById(roleId).orElseThrow(() -> new EntityException("No role found"));
+    }
+
+    public Role findByName(String name) {
+        return roleRepo.findByName(name).orElseThrow(() -> new EntityException("No role found"));
+    }
+
+    public void save(Role role) {
+        if (StringUtils.isEmpty(role.getName())) {
+            throw new EntityException("Property name is required!");
+        }
+        if (StringUtils.isEmpty(role.getId())) {
+            role.setId(UUIDGenerator.randomUUID());
+        }
+        roleRepo.save(role);
+    }
+
+    public void delete(Role role) {
+        roleRepo.delete(role);
+    }
+}
