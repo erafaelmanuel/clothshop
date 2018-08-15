9f503e8992d2ea51b42a4cd45fc2b438e744408f
Rafael Manuel <erafaelmanuel@gmail.com>
2018-08-15 12:03:31 +0800

    Update RestResponseEntityExceptionHandler

--- account-service/src/main/java/com/clothshop/accountservice/rest/RestResponseEntityExceptionHandler.java
+++ account-service/src/main/java/com/clothshop/accountservice/rest/RestResponseEntityExceptionHandler.java
@@ -2,6 +2,8 @@ package com.clothshop.accountservice.rest;
 
 import com.clothshop.accountservice.domain.Message;
 import com.clothshop.accountservice.exception.EntityConstraintViolationException;
+import com.clothshop.accountservice.exception.EntityException;
+import com.clothshop.accountservice.exception.RoleNotFoundException;
 import com.clothshop.accountservice.exception.UserNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -17,15 +19,23 @@ import java.util.Collections;
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler({EntityException.class})
+    public ResponseEntity<Object> handleEntityException(Exception e, WebRequest request) {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        return new ResponseEntity<>(new Message(500, e.getMessage()), headers, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler({EntityConstraintViolationException.class})
-    public ResponseEntity<Object> handleAccessDeniedException(Exception e, WebRequest request) {
+    public ResponseEntity<Object> handleEntityConstraintViolationException(Exception e, WebRequest request) {
         final HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 
         return new ResponseEntity<>(new Message(206, e.getMessage()), headers, HttpStatus.PARTIAL_CONTENT);
     }
 
-    @ExceptionHandler({UserNotFoundException.class})
+    @ExceptionHandler({UserNotFoundException.class, RoleNotFoundException.class})
     public ResponseEntity<Object> handleNotFoundException(Exception e, WebRequest request) {
         final HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
